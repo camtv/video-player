6c2274566da5c863b1ad9c4de2fcf31cba2d26d4
Ivan Prez <ivan.camtv@gmail.com>
2020-12-15 16:47:02 +0000

    Fixed the require error in building

--- src/components/translations.js
+++ src/components/translations.js
@@ -1,11 +1,20 @@
 import videojs from "video.js";
 
+// eslint-disable-next-line no-undef
 videojs.addLanguage("it", require("../../lang/it.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/en.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/de.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/es.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/fr.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/ru.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/fu.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/hu.json"));
+// eslint-disable-next-line no-undef
 videojs.addLanguage("en", require("../../lang/ro.json"));
